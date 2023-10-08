8697d2d8e88fafd363041077e771ab3db34abb9c
Jack W. <git@jack.cab>
2023-10-08 18:55:18 -0400

    feat: idk i need to get this out here

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "flake-utils";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable"; #nixpkgs-unstable"; # gnome";
     naersk.url = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
-   };
+  };
 
-  outputs = { self, flake-utils, naersk, nixpkgs}:
+  outputs = { self, flake-utils, naersk, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs) {
@@ -19,6 +19,7 @@
       {
         # For `nix build` & `nix run`:
         defaultPackage = naersk'.buildPackage {
+          pname = "tangent";
           src = ./.;
           nativeBuildInputs = [ pkgs.pkg-config ];
           buildInputs = [
@@ -27,7 +28,15 @@
             pkgs.libadwaita
             pkgs.glib-networking
             pkgs.p11-kit # Trying stuff until HTTPS works
+            pkgs.gst_all_1.gst-libav
+            pkgs.gst_all_1.gst-plugins-bad
+            pkgs.gst_all_1.gst-plugins-base
+            pkgs.gst_all_1.gst-plugins-good
+            pkgs.gst_all_1.gst-plugins-ugly
+            pkgs.gst_all_1.gstreamer
+            pkgs.wrapGAppsHook
           ];
+          meta.mainProgram = "tangent";
         };
 
         # For `nix develop`:
@@ -41,6 +50,13 @@
             pkgs.webkitgtk_6_0
             pkgs.gtk4
             pkgs.libadwaita
+            pkgs.gst_all_1.gst-libav
+            pkgs.gst_all_1.gst-plugins-bad
+            pkgs.gst_all_1.gst-plugins-base
+            pkgs.gst_all_1.gst-plugins-good
+            pkgs.gst_all_1.gst-plugins-ugly
+            pkgs.gst_all_1.gstreamer
+            pkgs.wrapGAppsHook
           ];
           RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
         };
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+use adw::glib::GString;
 use adw::prelude::*;
 use gtk::prelude::*;
-use relm4::prelude::*;
-use relm4::{
-    binding::{ConnectBindingExt, StringBinding},
-    gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent,
-};
+use relm4::{prelude::*, set_global_css, RelmObjectExt};
+
+use relm4::{gtk, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 use webkit6::{traits::*, WebView};
 
-struct AppModel {}
+#[tracker::track]
+struct AppModel {
+    url: String,
+}
 
 #[derive(Debug)]
 enum AppMsg {
-    Go,
     About,
+    Load(GString),
 }
 
 #[relm4::component]
@@ -51,9 +53,6 @@ impl SimpleComponent for AppModel {
                                 connect_clicked => AppMsg::About
                             },
                         },
-                        adw::EntryRow {
-                            set_title: "URL"
-                        },
                         gtk::Box {
                             set_valign: gtk::Align::Start,
                             set_vexpand: true,
@@ -65,14 +64,31 @@ impl SimpleComponent for AppModel {
                                 set_label: "Introducing Tangent - Etcetera",
                             },
                             gtk::Button {
-                                inline_css: "margin-left: 1ch;",
+                                inline_css: "margin-left: 16px;",
                                 set_label: "The WWWorst App Store",
                                 add_css_class: "link-button"
                             }
                         }
                     },
 
-                    WebView {
+                    gtk::Box {
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_valign: gtk::Align::Fill,
+                        set_vexpand: true,
+                        set_halign: gtk::Align::Fill,
+                        set_hexpand: true,
+                        #[name="url_entry"]
+                        gtk::Entry {
+                            set_placeholder_text: Some("URL"),
+                            add_css_class: "url-entry",
+                            add_css_class: "body",
+                            #[track = "model.changed(AppModel::url())"]
+                            set_text: &model.url,
+                            connect_activate[sender] => move |entry| {
+                                sender.input(AppMsg::Load(entry.text()))
+                            },
+                        },
+                     WebView {
                         // set_editable: true,
                         set_valign: gtk::Align::Fill,
                         set_vexpand: true,
@@ -83,7 +99,16 @@ impl SimpleComponent for AppModel {
                             settings.set_enable_developer_extras(true);
                             settings
                         }),
-                        load_uri: "https://en.wikipedia.org"//"http://127.0.0.1:3000/"
+
+                        // #[watch]
+                        #[block_signal(load_handle)]
+                        #[track = "model.changed(AppModel::url())"]
+                        load_uri: &model.url,//"https://duckduckgo.com/lite", //https://www.youtube.com/watch?v=V_VfRHS_K0o",//"http://127.0.0.1:3000/"
+
+                        connect_uri_notify[sender] => move |webview| {
+                            sender.input(AppMsg::Load(webview.uri().unwrap_or_default()))
+                        } @load_handle,
+                    }
                     }
                 }
             }
@@ -96,7 +121,10 @@ impl SimpleComponent for AppModel {
         root: &Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = AppModel {};
+        let model = AppModel {
+            url: "https://start.duckduckgo.com".into(),
+            tracker: 0,
+        };
 
         // Insert the macro code generation here
         let widgets = view_output!();
@@ -105,9 +133,10 @@ impl SimpleComponent for AppModel {
     }
 
     fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        self.reset();
         match msg {
-            AppMsg::Go => {
-                println!("Go!");
+            AppMsg::Load(uri) => {
+                self.set_url(uri.to_string());
             }
             AppMsg::About => {
                 let dialog = adw::AboutWindow::builder()
@@ -124,8 +153,12 @@ impl SimpleComponent for AppModel {
                     .license_type(gtk::License::Gpl30)
                     .version(option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
                     .issue_url("https://github.com/mollersuite/tangent/issues")
+                    .support_url("https://cetera.uk/discord")
                     .website("https://github.com/mollersuite/tangent") // for now
                     .build();
+                // dialog.connect_activate_link(|_, url| {
+                //     println!("Link clicked: {}", url);
+                // });
                 dialog.add_acknowledgement_section(
                     None,
                     &["Molly the Beagle, 2009-2022 🕊️ https://mola.pages.dev"],
@@ -138,5 +171,6 @@ impl SimpleComponent for AppModel {
 
 fn main() {
     let app = RelmApp::new("uk.cetera.Tangent");
+    set_global_css(include_str!("./style.css"));
     app.run::<AppModel>(());
 }
